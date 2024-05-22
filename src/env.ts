4e737c4c84e3dcdee5cce0467d057d17076877b1
devSaifur <saifur2akash@gmail.com>
2024-05-22 14:17:56 +0600

    rm comments

--- src/env.ts
+++ src/env.ts
@@ -11,7 +11,6 @@ export const env = createEnv({
     UPSTASH_VECTOR_REST_TOKEN: z.string().min(1),
   },
   client: {},
-  // If you're using Next.js < 13.4.4, you'll need to specify the runtimeEnv manually
   runtimeEnv: {
     DATABASE_URL: process.env.DATABASE_URL,
     UPLOADTHING_SECRET: process.env.UPLOADTHING_SECRET,
@@ -20,8 +19,4 @@ export const env = createEnv({
     UPSTASH_VECTOR_REST_URL: process.env.UPSTASH_VECTOR_REST_URL,
     UPSTASH_VECTOR_REST_TOKEN: process.env.UPSTASH_VECTOR_REST_TOKEN,
   },
-  // For Next.js >= 13.4.4, you only need to destructure client variables:
-  // experimental__runtimeEnv: {
-  //   NEXT_PUBLIC_PUBLISHABLE_KEY: process.env.NEXT_PUBLIC_PUBLISHABLE_KEY,
-  // }
 })
